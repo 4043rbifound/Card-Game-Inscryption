81cd9907d854ad3ad7be4d5161fd7e706812eadf
Mohamed Amine BOUZAIENE <mohamed-amine.bouzaiene@etu.unistra.fr>
2026-05-27 14:56:51 +0200

    création des classes InterfaceConsole, AnalyseurSaisie, Commande et GestionnairePartie

--- src/Inscryption/Affichage/GestionnairePartie.java
+++ src/Inscryption/Affichage/GestionnairePartie.java
@@ -1,4 +1,115 @@
 package Inscryption.Affichage;
 
+
+import inscryption.logique.AdversaireLogic;
+import inscryption.logique.CarteLogic;
+import inscryption.logique.JoueurLogic;
+import inscryption.logique.PlanJeu;
+import inscryption.logique.PlateauLogic;
+import inscryption.logique.ScoreLogic;
+
 public class GestionnairePartie {
+
+    private PlateauLogic m_plateau;
+    private JoueurLogic m_joueur;
+    private AdversaireLogic m_adversaire;
+    private ScoreLogic m_score;
+
+    private InterfaceConsole m_vue;
+    private AnalyseurSaisie m_analyseur;
+
+    private int m_tour;
+
+    public GestionnairePartie() {
+
+        this.m_plateau = new PlateauLogic();
+        this.m_joueur = new JoueurLogic();
+        this.m_adversaire = new AdversaireLogic(new PlanJeu());
+        this.m_score = new ScoreLogic();
+
+        this.m_vue = new InterfaceConsole();
+        this.m_analyseur = new AnalyseurSaisie();
+
+        this.m_tour = 1;
+    }
+
+    public void executerJeu() {
+
+        while (!this.m_score.estPartieTerminee()) {
+
+            CarteLogic[] intentions = this.m_adversaire.obtenirIntention(this.m_tour);
+
+            this.m_vue.afficherEtatJeu(
+                    this.m_plateau,
+                    this.m_joueur,
+                    this.m_score,
+                    this.m_tour,
+                    intentions);
+
+            boucleTourJoueur();
+
+            this.m_adversaire.jouerTour(this.m_tour, this.m_plateau);
+
+            this.m_tour++;
+        }
+
+        System.out.println();
+        System.out.println("Partie terminée !");
+        System.out.println("Gagnant : " + this.m_score.getGagnant());
+    }
+
+    private void boucleTourJoueur() {
+
+        boolean tourTermine = false;
+
+        while (!tourTermine) {
+
+            String saisie = this.m_vue.lireSaisie();
+
+            Commande commande = this.m_analyseur.interpreter(saisie);
+
+            if (commande == null) {
+                System.out.println("Commande invalide.");
+                continue;
+            }
+
+            String action = commande.getAction();
+
+            if (action.equals("fin")) {
+
+                tourTermine = true;
+            }
+
+            else if (action.equals("piocher")) {
+
+                this.m_joueur.piocherCartePrincipal();
+                System.out.println("Carte piochée.");
+            }
+
+            else if (action.equals("placer")) {
+
+                int indexCarte = commande.getIndexCarteMain();
+                int position = commande.getPositionPlateau();
+
+                System.out.println(
+                        "Placement de la carte "
+                                + indexCarte
+                                + " en position "
+                                + position);
+
+                /*
+                 * Ici vous ajouterez plus tard :
+                 * - récupération de la carte
+                 * - vérification du coût
+                 * - placement sur le plateau
+                 */
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        GestionnairePartie gestionnaire = new GestionnairePartie();
+        gestionnaire.executerJeu();
+    }
 }
--- src/Inscryption/Affichage/InterfaceConsole.java
+++ src/Inscryption/Affichage/InterfaceConsole.java
@@ -1,4 +1,129 @@
 package Inscryption.Affichage;
 
+import java.util.Scanner;
+import inscryption.logique.CarteAnimalLogic;
+import inscryption.logique.CarteLogic;
+import inscryption.logique.Emplacement;
+import inscryption.logique.JoueurLogic;
+import inscryption.logique.PlateauLogic;
+import inscryption.logique.ScoreLogic;
+
 public class InterfaceConsole {
+
+    private Scanner m_scanner;
+
+    public InterfaceConsole() {
+        this.m_scanner = new Scanner(System.in);
+    }
+
+    public void afficherEtatJeu(
+            PlateauLogic plateau,
+            JoueurLogic joueur,
+            ScoreLogic score,
+            int tour,
+            CarteLogic[] intentions) {
+
+        System.out.println();
+        System.out.println("============================");
+        System.out.println("TOUR " + tour);
+        System.out.println("============================");
+
+        System.out.println("Score : " + score.getValeurRelative());
+        System.out.println();
+
+        System.out.println("Intentions adverses : ");
+
+        for (int i = 0; i < intentions.length; i++) {
+
+            if (intentions[i] == null) {
+                System.out.print("[VIDE] ");
+            } else {
+                System.out.print("[" + intentions[i].getNom() + "] ");
+            }
+        }
+
+        System.out.println();
+        System.out.println();
+
+        afficherPlateau(plateau);
+
+        System.out.println();
+
+        afficherMain(joueur);
+
+        System.out.println();
+
+        System.out.println("Commandes possibles :");
+        System.out.println("- piocher");
+        System.out.println("- fin");
+        System.out.println("- placer <indexCarte> <position>");
+    }
+
+    public void afficherPlateau(PlateauLogic plateau) {
+
+        Emplacement[] casesAdversaire = plateau.getCasesAdversaire();
+        Emplacement[] casesJoueur = plateau.getCasesJoueur();
+
+        System.out.println("Plateau adverse :");
+
+        for (int i = 0; i < casesAdversaire.length; i++) {
+
+            CarteLogic carte = casesAdversaire[i].getCarteContenue();
+
+            if (carte == null) {
+                System.out.print("[VIDE] ");
+            } else {
+                System.out.print("[" + carte.getNom() + " PV:" + carte.getPointsVieActuels() + "] ");
+            }
+        }
+
+        System.out.println();
+
+        System.out.println("Plateau joueur :");
+
+        for (int i = 0; i < casesJoueur.length; i++) {
+
+            CarteLogic carte = casesJoueur[i].getCarteContenue();
+
+            if (carte == null) {
+                System.out.print("[VIDE] ");
+            } else {
+                System.out.print("[" + carte.getNom() + " PV:" + carte.getPointsVieActuels() + "] ");
+            }
+        }
+
+        System.out.println();
+    }
+
+    public void afficherMain(JoueurLogic joueur) {
+
+        System.out.println("Main du joueur :");
+
+        for (int i = 0; i < joueur.getMain().length; i++) {
+
+            CarteAnimalLogic carte = joueur.getMain()[i];
+
+            System.out.println(
+                    (i + 1)
+                            + ". "
+                            + carte.getNom()
+                            + " | PV: "
+                            + carte.getPointsVieActuels()
+                            + " | ATT: "
+                            + carte.getPointsAttaque());
+        }
+    }
+
+    public void afficherMessages(String[] messages) {
+
+        for (int i = 0; i < messages.length; i++) {
+            System.out.println(messages[i]);
+        }
+    }
+
+    public String lireSaisie() {
+
+        System.out.print("> ");
+        return this.m_scanner.nextLine();
+    }
 }
